85d1b1377fa1ade98dfa9f2b68024afa4babc8ff
Monika Palova <44198558+MonikaPalova@users.noreply.github.com>
2021-06-07 23:08:44 +0300

    add cors

--- src/main/java/bg/bilet4e/prototype/user/customer/rest/CustomerController.java
+++ src/main/java/bg/bilet4e/prototype/user/customer/rest/CustomerController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import bg.bilet4e.prototype.user.customer.CustomerService;
 
 @RestController
 @RequestMapping(value = CustomerController.API_BASE_PATH)
+@CrossOrigin(origins = { "http://localhost:4200", "https://bilet4e.herokuapp.com" }, allowedHeaders="*")
 class CustomerController {
 
     static final String API_BASE_PATH = "api/v1/customers";
--- src/main/java/bg/bilet4e/prototype/user/owner/rest/OwnerController.java
+++ src/main/java/bg/bilet4e/prototype/user/owner/rest/OwnerController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import bg.bilet4e.prototype.user.owner.OwnerService;
 
 @RestController
 @RequestMapping(value = OwnerController.API_BASE_PATH)
+@CrossOrigin(origins = { "http://localhost:4200", "https://bilet4e.herokuapp.com" }, allowedHeaders="*")
 class OwnerController {
 
     static final String API_BASE_PATH = "api/v1/owners";
