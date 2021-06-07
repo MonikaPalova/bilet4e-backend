5a28c5bf54d711946a14fbdd8109441b4402cd8c
Monika Palova <44198558+MonikaPalova@users.noreply.github.com>
2021-06-07 20:15:58 +0300

    add cors filter

--- src/main/java/bg/bilet4e/prototype/PrototypeApplication.java
+++ src/main/java/bg/bilet4e/prototype/PrototypeApplication.java
@@ -1,13 +1,35 @@
 package bg.bilet4e.prototype;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class PrototypeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PrototypeApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Don't do this in production, use a proper list of allowed origins
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(
+                Arrays.asList("Origin", "Content-Type", "Accept", "Authorization", "xsrf-token"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        config.setExposedHeaders(Arrays.asList("Authorization"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
--- src/main/java/bg/bilet4e/prototype/security/WebSecurityConfig.java
+++ src/main/java/bg/bilet4e/prototype/security/WebSecurityConfig.java
@@ -54,6 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.csrf().disable() //
+                .cors().and() //
 
                 .authorizeRequests() //
                 .antMatchers(AUTHENTICATE_PATH, REGISTER_PATH).permitAll() //
